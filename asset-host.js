34169e06eefb549cca9ae4387f05e0b6ed12baa6
Igu2012 <igormombergdemeira@gmail.com>
2026-09-08 18:54:31 +0000

    Add Spaceflight Pizza Tower Peak and The Visitor

--- asset-host.js
+++ asset-host.js
@@ -20,6 +20,9 @@
   };
 
   const repositoryByGame = {
+    Peak: 'gamefiles03',
+    SpaceflightSimulator: 'gamefiles03',
+    PizzaTower: 'gamefiles03',
     HollowKnightSilksong: 'gamefiles01', Cuphead: 'gamefiles01', Balatro: 'gamefiles01',
     CookieClicker: 'gamefiles01', BaldiRemastered: 'gamefiles01', Antonblast: 'gamefiles03', Brotato: 'gamefiles03', ButchersAround: 'gamefiles03', Slendytubbies: 'gamefiles03', Slendytubbies2: 'gamefiles03', Slendytubbies2D: 'gamefiles03', SoccerRandom: 'gamefiles01',
     RocketGoal: 'gamefiles01', BasketRandom: 'gamefiles01', VolleyRandom: 'gamefiles01',
